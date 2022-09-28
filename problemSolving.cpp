e4c67517da1ed587a3c157616bc74adec8f60be5
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2022-09-28 19:50:49 +0000

    fixed code in problemSolving.cpp

--- problemSolving.cpp
+++ problemSolving.cpp
@@ -15,6 +15,5 @@ int main() {
     std::cout << std::endl;
     std::cout << "Area is " << (M_PI * pow(15, 2)) << " mm²." << std::endl;
     std::cout << "Perimeter is " << (2 * M_PI * 15) << " mm." << std::endl;
-    std::cout << std::endl;
-    std::cout << "Done." << std::endl;
+    std::cout << "\nDone." << std::endl;
 }
